5e029c7ad066e572f1c87ab0d266d748fd819bc1
XiaoYhun <xiaoyhun@gmail.com>
2023-07-07 01:05:45 +0700

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
+import { Apps } from "@mui/icons-material";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { ErrorComponent, notificationProvider, RefineSnackbarProvider, ThemedLayoutV2 } from "@refinedev/mui";
+import {
+  ErrorComponent,
+  notificationProvider,
+  RefineSnackbarProvider,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+} from "@refinedev/mui";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
-import routerBindings, {
-  DocumentTitleHandler,
-  NavigateToResource,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
+import routerBindings, { DocumentTitleHandler, UnsavedChangesNotifier } from "@refinedev/react-router-v6";
 import { dataProvider } from "./rest-data-provider";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Header } from "./components/header";
 import { ColorModeContextProvider } from "./contexts/color-mode";
-import { ClientList, BlogPostShow, ClientEdit, ClientCreate } from "./pages/client";
-import { useEffect } from "react";
-import { MuiCreateInferencer } from "@refinedev/inferencer/mui";
+import { ClientList, ClientEdit, ClientCreate } from "./pages/client";
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
                   create: "/clients/create",
                   edit: "/clients/edit/:id",
                   meta: {
+                    icon: <Apps />,
                     canDelete: true,
                   },
                 },
@@ -44,12 +46,16 @@ function App() {
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
+                breadcrumb: true,
               }}
             >
               <Routes>
                 <Route
                   element={
-                    <ThemedLayoutV2 Header={() => <Header sticky />}>
+                    <ThemedLayoutV2
+                      Header={() => <Header sticky />}
+                      Sider={() => <ThemedSiderV2 Title={() => "KyberSwap IAM Admin"} />}
+                    >
                       <Outlet />
                     </ThemedLayoutV2>
                   }
